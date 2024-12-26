42d2dfc2dd8fdd0d10fd96e783ef6315bc3f5380
sayan706 <sayanchatterjee706@gmail.com>
2024-12-26 15:15:38 +0530

    feat(case ~ views): Implemented case creation api

--- app/v1/case/views.py
+++ app/v1/case/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from app.models import Case, Patient
+from app.models import Case, Patient , Doctor , CaseChiefComplaint, CaseDocument, CaseFinding
 from app.serializers.case import CaseSerializer
 
 
@@ -91,3 +91,94 @@ class CaseAPIView(APIView):
       message=message,
       data=data
     )
+
+  def post(self, request, format=None):
+    doctor_id = request.data.get('doctor_id')
+    patient_id = request.data.get('patient_id')
+
+    if not (doctor_id and patient_id):
+      raise exceptions.GenericException(
+        detail='Provide doctor_id & patient_id in request body',
+        code='Doctor and patient identifiers missing'
+      )
+    
+    try:
+      doctor = Doctor.objects.get(id=doctor_id)
+    except Doctor.DoesNotExist:
+      raise exceptions.DoesNotExistException(
+          detail=f'No doctor found with id {doctor_id}',
+          code='Doctor not found'
+        )
+    
+    try:
+      patient = Patient.objects.get(id=patient_id)
+    except Patient.DoesNotExist:
+      raise exceptions.DoesNotExistException(
+          detail=f'No patient found with id {patient_id}',
+          code='Patient not found'
+        )
+    
+    case_data = {
+      'past_treatment': request.data.get('past_treatment'),
+      'treatment_location': request.data.get('treatment_location'),
+      'treatment_type': request.data.get('treatment_type'),
+      'note': request.data.get('note'),
+      'follow_up_id': request.data.get('follow_up_id'),
+      'follow_up_date': request.data.get('follow_up_date'),
+      'assigned_doctor': doctor,
+      'patient': patient
+    }
+
+    case = Case.objects.create(**case_data)
+
+    # for case findings, and documents and chief complaints
+
+    chief_complaints = request.data.get('chief_complaints', [])
+    for complaint in chief_complaints:
+        CaseChiefComplaint.objects.create(
+            case=case,
+            title=complaint.get('title'),
+            severity=complaint.get('severity'),
+            duration=complaint.get('duration'),
+            duration_unit=complaint.get('duration_unit')
+        )
+  
+    findings = request.data.get('findings', [])
+    for finding in findings:
+        CaseFinding.objects.create(case=case, title=finding)
+
+    documents = request.data.get('documents', [])
+    for document in documents:
+      CaseDocument.objects.create(
+        case=case,
+        file_name=document.get('file_name'),
+        file_extension=document.get('file_extension'),
+        file_url=document.get('file_url'),
+        document_section=document.get('document_section')
+      )
+
+    serialized_case = CaseSerializer(
+      instance=case,
+      fields=[
+      'id',
+      'patient',
+      'assigned_doctor',
+      'chief_complaints',
+      'findings',
+      'documents',
+      'past_treatment',
+      'treatment_location',
+      'treatment_type',
+      'note',
+      'follow_up_id',
+      'follow_up_date',
+      'created_at'
+      ]
+    )
+
+    return custom_response_handler(
+      status=status.HTTP_201_CREATED,
+      message="Case created successfully",
+      data=serialized_case.data
+    )
+    
