1ca5a00633be4850a52f1b92d369c06e57cd0d7f
supratim531 <91423964+supratim531@users.noreply.github.com>
2025-02-07 01:53:58 +0530

    supratim531: fix(medicine ~ views): Limit first 50 medicine_names for both normal and filtered data

--- app/v1/medicine/views.py
+++ app/v1/medicine/views.py
@@ -19,8 +19,8 @@ from app.models import (
 
 class MedicineStuffsAPIView(APIView):
   def get(self, request, *args, **kwargs):
-    type_param = request.query_params.get("type")
-    search_param = request.query_params.get("search", "").strip()
+    type_param = request.query_params.get('type')
+    search_param = request.query_params.get('search', '').strip()
 
     if not type_param:
       raise exceptions.GenericException(
@@ -29,25 +29,26 @@ class MedicineStuffsAPIView(APIView):
       )
 
     model_mapping = {
-      "name": (MedicineName, MedicineNameSerializer, "name"),
-      "dose_type": (MedicineDoseType, MedicineDoseTypeSerializer, "name"),
-      "dose_quantity": (MedicineDoseQuantity, MedicineDoseQuantitySerializer, "quantity"),
-      "dose_regimen": (MedicineDoseRegimen, MedicineDoseRegimenSerializer, "name"),
-      "dose_duration": (MedicineDoseDuration, MedicineDoseDurationSerializer, "duration"),
+      'name': (MedicineName, MedicineNameSerializer, 'name'),
+      'dose_type': (MedicineDoseType, MedicineDoseTypeSerializer, 'name'),
+      'dose_quantity': (MedicineDoseQuantity, MedicineDoseQuantitySerializer, 'quantity'),
+      'dose_regimen': (MedicineDoseRegimen, MedicineDoseRegimenSerializer, 'name'),
+      'dose_duration': (MedicineDoseDuration, MedicineDoseDurationSerializer, 'duration'),
     }
 
     if type_param not in model_mapping:
       raise exceptions.GenericException(
-        detail='Invalid type parameter',
-        code='Invalid parameter'
+        detail='Invalid type parameter (valid: name | dose_type | dose_quantity | dose_regimen | dose_duration)',
+        code='Invalid parameter value'
       )
 
     model, serializer_class, search_field = model_mapping[type_param]
     queryset = model.objects.filter(is_active=True)
 
     if search_param:
-      queryset = queryset.filter(Q(**{f"{search_field}__icontains": search_param}))
+      queryset = queryset.filter(Q(**{f'{search_field}__icontains': search_param}))
 
+    queryset = queryset[:50] if type_param == 'name' else queryset
     serializer = serializer_class(queryset, many=True)
 
     return custom_response_handler(
